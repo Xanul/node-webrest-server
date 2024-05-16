9ced87a50442f18d744bddb9e99fb7862ba423e8
Xanul <rivas.060@gmail.com>
2024-05-16 14:33:33 -0600

    Changed the envs options on app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { envs } from "./config/envs";
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
 
@@ -8,8 +9,8 @@ import { Server } from "./presentation/server";
 function main() {
 
   const server = new Server({
-    port: 3000, 
-    public_path: 'public',
+    port: envs.PORT, 
+    public_path: envs.PUBLIC_PATH,
     routes: AppRoutes.routes
   });
   server.start();
