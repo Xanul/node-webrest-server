3c81d15fd03915a9a634b793f03a9d6442076270
Xanul <rivas.060@gmail.com>
2024-05-16 13:50:37 -0600

    Finished the basic CRUD for the API

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 
 const todos = [
-  {id: 1, title: 'Todo 1', createdAt: new Date()},
-  {id: 2, title: 'Todo 2', createdAt: new Date()},
-  {id: 3, title: 'Todo 3', createdAt: new Date()},
-  {id: 4, title: 'Todo 4', createdAt: new Date()},
-  {id: 5, title: 'Todo 5', createdAt: new Date()},
+  {id: 1, title: 'Todo 1', completedAt: new Date()},
+  {id: 2, title: 'Todo 2', completedAt: new Date()},
+  {id: 3, title: 'Todo 3', completedAt: new Date()},
+  {id: 4, title: 'Todo 4', completedAt: new Date()},
+  {id: 5, title: 'Todo 5', completedAt: new Date()},
 ];
 
 export class TodosController {
@@ -30,9 +30,50 @@ export class TodosController {
 
   public createTodo = (req:Request, res:Response) => {
 
-    const body = req.body;
-    return res.json(body);
+    const { title } = req.body;
+    if (!title) return res.status(400).json({message: 'Title is required!'});
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      completedAt: null!
+    }
+    todos.push(newTodo);
+    return res.json(newTodo);
     
+  };
+
+  public updateTodo = (req:Request, res:Response) => {
+
+    const id = +req.params.id;
+    if (isNaN(id)) return res.status(400).json({message: 'Invalid id!'});
+
+    const foundTodo = todos.find(todo => todo.id === id);
+    if (!foundTodo) return res.status(404).json({message: `Todo with id ${id} not found!`});
+
+    const { title, completedAt } = req.body;
+    // if (!title) return res.status(400).json({message: 'Title is required!'});
+
+    // Se actualiza el todo por referencia
+    foundTodo.title = title || foundTodo.title;
+    (completedAt === null)
+      ? foundTodo.completedAt = null!
+      : foundTodo.completedAt = new Date(completedAt || foundTodo.completedAt);
+    return res.json(foundTodo);
+
+  };
+
+  public deleteTodo = (req:Request, res:Response) => {
+
+    const id = +req.params.id;
+    if (isNaN(id)) return res.status(400).json({message: 'Invalid id!'});
+
+    const todoToDelete = todos.find(todo => todo.id === id);
+    if (!todoToDelete) return res.status(400).json({message: `Todo not found with id ${id}`});
+
+    const indexToDelete = todos.findIndex(todo => todo.id === id);
+    todos.splice(indexToDelete, 1);
+
+    return res.json(todoToDelete);
 
   };
 
